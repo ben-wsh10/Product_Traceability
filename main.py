a18169fd4eeda0899b93405282a19ae26723cb84
Ben_wsh10 <benwu1997@gmail.com>
2022-01-11 13:22:16 +0800

    Read CSV functionality

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import os
 import sys
 import Controller
 from PyQt5.QtCore import QTime
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog, QTableWidgetItem
 import csv
+import pandas as pd
 
 from Trace import Ui_MainWindow
+
+path = ""
 
 
 class Main(QMainWindow, Ui_MainWindow):
@@ -21,8 +24,12 @@ class Main(QMainWindow, Ui_MainWindow):
         self.createFileButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
         self.backButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
         self.backButton2.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
+        self.backButton3.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
+        self.backButton3.clicked.connect(lambda: self.excelTable.clear())
         self.importExcelButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(2))
         self.openFileDialogButton.clicked.connect(lambda: self.openFileDialog())
+        self.openFileButton.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(3))
+        self.openFileButton.clicked.connect(lambda: self.populateTable())
 
     def createCSV(self):
         # Get CSV name
@@ -44,6 +51,8 @@ class Main(QMainWindow, Ui_MainWindow):
         msg.exec_()
 
     def openFileDialog(self):
+        global path
+
         directory = os.path.dirname(__file__)
         path = QFileDialog.getOpenFileName(self, "Import File", directory, 'All Files (*.*)')
 
@@ -55,6 +64,20 @@ class Main(QMainWindow, Ui_MainWindow):
             msg.setWindowTitle("Error!")
             msg.setText("Invalid file type. Please select another file.")
             msg.exec_()
+
+    def populateTable(self):
+        global path
+
+        df = pd.read_csv(path[0])
+        rowCount = len(df.index)
+        columnCount = len(df.columns)
+        self.excelTable.setColumnCount(columnCount)
+        self.excelTable.setRowCount(rowCount)
+        self.excelTable.setHorizontalHeaderLabels(df.columns)
+
+        for i in range(rowCount):
+            for j in range(columnCount):
+                self.excelTable.setItem(i, j, QTableWidgetItem(str(df.iat[i, j])))
 
 
 if __name__ == '__main__':
